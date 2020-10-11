c330d8a437f76343cebbafdc8959690f33006ced
Olav Schettler <olav.schettler@dw.com>
2020-10-11 08:56:44 +0200

    Web views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Providers\RouteServiceProvider;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if (Auth::id()) {
+        return redirect(RouteServiceProvider::HOME);
+    }
     return view('welcome');
 });
 
